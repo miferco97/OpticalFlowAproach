c30c64651c03e08dc6fcff9a261aeff4f1df5e4b
Miguel <miferco97@gmail.com>
2019-12-23 16:46:22 +0100

    Homography filter bug solved

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,5 @@
 from HomographyFilter import *
+import cv2
 import os
 
 # Take first frame and find corners in it
@@ -13,10 +14,10 @@ filter1 = HomographyFilter()
 
 for i, names in enumerate(elements):
 
-    frame=cv.imread(PATH+names)
-    resultMask = filter(frame)
-    cv.imshow('frame1',resultMask)
-    k = cv.waitKey() & 0xff
+    frame=cv2.imread(PATH+names)
+    resultMask = filter1(frame)
+    cv2.imshow('frame1',resultMask)
+    k = cv2.waitKey() & 0xff
     if k == 27:
          break
 
